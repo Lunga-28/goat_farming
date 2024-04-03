5e28507bb85361cffbcbe36dcf322a34cc7bccb4
Mphare1 <thipanemp@gmail.com>
2024-04-03 09:44:46 +0200

    minor adjusment to UI

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -11,44 +11,60 @@ class MyDrawer extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.background,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [Column(
-          children: [
-            // do Logo
-            DrawerHeader(child: Center(child: Image.asset('assets/images/logo.jpg'),
-            ),
-            ),
-            //Home list tittle
-            Padding(padding: const EdgeInsets.only(left:25.0),
-            child: ListTile(title:const Text("H O M E"),
-            leading:const Icon(Icons.home),
-            onTap: () {
-              Navigator.pop(context);
-            },
-            ),
-            ),
+        children: [
+          Column(
+            children: [
+              // do Logo
+              DrawerHeader(
+                child: Center(
+                  child: Image.asset('assets/images/logo.jpg'),
+                ),
+              ),
+              //Home list tittle
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: const Text("H O M E"),
+                  leading: const Icon(Icons.home),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
 //settings tittle
-Padding(padding: const EdgeInsets.only(left: 25.0),
-child: ListTile(title: const Text("S E T T I N G S"),
-leading:const Icon(Icons.settings_applications),
-onTap: () {
-  Navigator.pop(context);
-  Navigator.push(context, MaterialPageRoute(
-    builder: (context) =>const SettingsPage(),
-  ),);
-},),),
 
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: const Text("S E T T I N G S"),
+                  leading: const Icon(Icons.settings_applications),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SettingsPage(),
+                      ),
+                    );
+                  },
+                ),
+              ),
             ],
-        ),
-     //Logout list Tittle
-     Padding(padding: const EdgeInsets.only(left: 25.0, bottom:  25),
-     child: ListTile(title:const Text("L O G O U T"),
-     leading:const Icon(Icons.logout_rounded),
-     onTap: (){
-          final auth = AuthService();
-    auth.signOut();
-     },),)   
-      ],),
-      
+          ),
+          //Logout list Tittle
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, bottom: 25),
+            child: ListTile(
+              title: const Text("L O G O U T"),
+              leading: const Icon(Icons.logout_rounded),
+              onTap: () {
+                final auth = AuthService();
+                auth.signOut();
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
